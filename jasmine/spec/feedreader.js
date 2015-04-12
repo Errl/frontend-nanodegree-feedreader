50aa8f9264dddcb98276a5be07c5b83245ddba11
Hanane Moshe <hananemo@gmail.com>
2015-04-12 09:34:07 -0400

    made fixes and added comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,10 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+
+        /* Fixed length from not.toBe(0) to toBeGreaterThan(0)*/
         it('has url', function () {
             allFeeds.forEach(function (feed) {
                 expect(feed.url).toBeDefined();
-                expect(feed.url).not.toBe(0);
+                expect(feed.url.length).toBeGreaterThan(0);
             });
         });
 
@@ -45,7 +47,7 @@ $(function() {
         it('has name', function () {
             allFeeds.forEach(function (feed) {
                 expect(feed.name).toBeDefined();
-                expect(feed.name).not.toBe(0);
+                expect(feed.name.length).toBeGreaterThan(0);
             });
         });
     });
@@ -59,6 +61,7 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        /*This checks to see if the body has a clas of menu-hidden, then we know the menu is hidden*/
         it('is hidden', function () {
             expect($("body").hasClass("menu-hidden")).toBe(true);
         });
@@ -68,6 +71,7 @@ $(function() {
          * clicked and does it hide when clicked again.
          */
 
+        /*We have two expectations. we click the menu icon and see if the menu-hodden class has been removed from body. Then we click the icon again and make sure it was put back.*/
         it('shows menu when clicked', function () {
             var menuIcon = $('.menu-icon-link');
             menuIcon.trigger("click");
@@ -91,45 +95,46 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+        /*Here we load the first feed in the allFeeds array*/
         beforeEach(function (done) {
             loadFeed(0, function () {
                 done();
             });
         });
-
+        /*Here we make sure that the feed entry is not zero and it contains a feed*/
         it('have at least 1 entry', function () {
             expect($(".feed .entry-link").length).not.toBe(0);
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    ///* TODO: Write a new test suite named "New Feed Selection" */
     describe('New feed selection', function() {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-        * by the loadFeed function that the content actually changes.
-        * Remember, loadFeed() is asynchronous.
-        */
-
+    //    /* TODO: Write a test that ensures when a new feed is loaded
+    //    * by the loadFeed function that the content actually changes.
+    //    * Remember, loadFeed() is asynchronous.
+    //    */
+        var feed;
+        /*First we set the variable feed to the first entry in the feed list. 
+         * Then we call the loadFeed function and load the second feed in our all feeds array.
+         */
         beforeEach(function (done) {
-            loadFeed(0, function () {
-                done();
-            });
+            // get the content of the first feed entry
+            feed = $('.feed .entry-link').first().text();
+            // make a call to the css tricks feed url
+            loadFeed(1, done);
         });
-
-        it('feed contents change', function (done) {
-            var existingFeed = $('.feed').find('.entry');
-            loadFeed(1, function () {
-                var newFeed = $('.feed').find('.entry');
-
-                // Verify content exists
-                expect(newFeed.length).not.toBe(0);
-
-                // Verify the content has changed
-                expect(newFeed).not.toEqual(existingFeed);
-
-                done();
-            });
+        /*Now we load the first entry of the new feed and make sure it doesnt match the first entry of the old feed*/
+        it("feed content has changed", function (done) {
+            // get the content of the first feed entry AFTER the call
+            var newFeed = $('.feed .entry-link').first().text();
+            // check that both items are not matching
+            expect(feed).not.toMatch(newFeed);
+            done();
+        });
+        /*Here I execute the loadFeed function on the first feed in the allFeeds array, to set the home page back to the original feed.*/
+        afterAll(function (done) {
+            loadFeed(0, done);
         });
     });
 }());
